942e8982f33fbfc1c9ff391160199fd9e6acbcc2
karma19950807 <kasamdrup1995@gmail.com>
2021-10-25 10:19:29 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, EmployeeDashboardComponent],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private url: string;
+  constructor(private http: HttpClient) {
+    this.url = 'http://localhost:3000/posts';
+  }
+
+  postEmployee(data: any) {
+    return this.http.post<any>(this.url, data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  getEmployee() {
+    return this.http.get<any>(this.url).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  updateEmployee(data: any, id: number) {
+    return this.http.put<any>(this.url + '/' + id, data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  deleteEmployee(id: number) {
+    return this.http.delete<any>(this.url + '/' + id).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+}
